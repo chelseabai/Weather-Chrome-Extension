c1ec4b65ef82c4b491ffcd8f95a6e5fc6394ff16
Chelsea <baixinyu1998@gmail.com>
2020-08-01 22:35:47 +0800

    edits

--- Weather-extension/popup.js
+++ Weather-extension/popup.js
@@ -13,7 +13,7 @@ function weather(lat,lon){
             const country = (data.sys.country);
             const weather = (data.weather[0].main);
             document.querySelector('h2').innerHTML=`${city}, ${country}`;
-            document.querySelector('.weather').innerHTML= `Current Weather: ${weather}`;git
+            document.querySelector('.weather').innerHTML= `Current Weather: ${weather}`;
         })
 }
 // function city(lat,lon){
@@ -35,8 +35,7 @@ function Main(){
 function success(position) {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
-    let accuracy = position.coords.accuracy;
-    console.log(lat,lon,accuracy);
+    console.log(lat,lon;
     weather(lat,lon);
     // city(lat,lon);
 }
@@ -54,6 +53,5 @@ const options = {
 
 document.addEventListener('DOMContentLoaded',()=>{
     Main();
-
 });
 
