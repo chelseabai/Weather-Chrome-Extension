a76b2069a18b79a656f01b8c43b914301ac3541f
Ana Zainal <tarzanazainal@gmail.com>
2020-08-01 15:11:39 +0100

    added options button

--- Weather-extension/popup.js
+++ Weather-extension/popup.js
@@ -58,3 +58,11 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 });
 
+document.querySelector('#go-to-options').addEventListener("click", function() {
+    chrome.runtime.openOptionsPage();
+    // if (chrome.runtime.openOptionsPage) {
+    //     chrome.runtime.openOptionsPage();
+    //   } else {
+    //     window.open(chrome.runtime.getURL('options.html'));
+    //   }
+  });
